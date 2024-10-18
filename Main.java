5f99d06927d70318998a1cbe7f7e1debb6f25b0d
Ignacio Jose Calero Villalobos <ignacio.calero@ucr.ac.cr>
2024-10-18 17:43:50 -0600

    Se creo el main

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        JuegoControlador juego = new JuegoControlador();
+        juego.seleccionarJuego();
+        juego.jugar();
+    }
+}
